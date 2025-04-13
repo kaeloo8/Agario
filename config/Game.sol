b40e1d1ae048847d4c982271f5674417d1af85d9
kaeloo <147034168+kaeloo8@users.noreply.github.com>
2025-04-13 19:03:20 +0200

    new server

--- config/Game.sol
+++ config/Game.sol
@@ -1,10 +1,11 @@
 {
-    "solution_name": "Game",
     "format_version": "12.00",
-    "version": "16",
-    "version_full": "16.0.30319.14",
     "minimum_version": "10.0.40219.1",
     "projects": [
-        "Game.prj"
-    ]
+        "Game.prj",
+        "server.prj"
+    ],
+    "solution_name": "Game",
+    "version": "16",
+    "version_full": "16.0.30319.14"
 }
---
+++ src/server/main.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "main.h"
+
+int main()
+{
+#ifdef _DEBUG
+	_CrtDumpMemoryLeaks();
+#endif
+
+	//le server
+
+	return 0;
+}
